1117d074f335f896ebed1cf7212e8322f1ff298f
Juliette Sabatier <jsabatier001@ensc.fr>
2022-10-07 14:29:43 +0200

    chgmt1

--- EFTodo/Data/EFTodo.cs
+++ EFTodo/Data/EFTodo.cs
@@ -1,7 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
-public class BloggingContext : DbContext
+public class TodoContext : DbContext
 {
-    
+   public DbSet<Todo> Todo{get;set;}
+   public string DbPath {get; private set;} 
+
+   public TodoContext()
+    {
+        // Path to SQLite database file
+        DbPath = "EFTodo.db";
+    }
+    // The following configures EF to create a SQLite database file locally
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // Use SQLite as database
+        options.UseSqlite($"Data Source={DbPath}");
+        // Optional: log SQL queries to console
+        options.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information);
+    }
 }
