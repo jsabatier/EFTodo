246f50d8c24df52b6e97243d5968a27e274a960d
Juliette Sabatier <jsabatier001@ensc.fr>
2022-10-07 16:29:33 +0200

    update LINQ

--- EFTodo/Program.cs
+++ EFTodo/Program.cs
@@ -1,25 +1,37 @@
 ﻿using (var context = new TodoContext())
 {
-    Console.WriteLine("--- Inserting a new todo ---");
+    /*Console.WriteLine("--- Inserting a new todo ---");
     context.Add(new Todo{Task ="Faire le ménage",Completed=false});
+    context.SaveChanges();
     Console.WriteLine("--- Inserting a new todo ---");
     context.Add(new Todo{Task ="Sortir les poubelles",Completed=false});
+    context.SaveChanges();
     Console.WriteLine("--- Inserting a new todo ---");
     context.Add(new Todo{Task ="Aller acheter un chat",Completed=true});
-    context.SaveChanges();
-
-    /*Console.WriteLine("--- Retrieve teh todo with lowest id ---");
-    var todo = context.Todo
-        .OrderBy(b => b.Id)
-        .First();
-
-    Console.WriteLine("--- Updating the blog and adding a post ---");
-    todo.Task = "Faire le ménage";
-    
-    
-    context.SaveChanges();
-
-    Console.WriteLine("--- Deleting the blog ---");
-    //context.Remove(todo);
     context.SaveChanges();*/
+
+    var todos = context.Todo
+            .ToList();
+    
+    //Console.WriteLine("--- Liste de tout les todos---");
+    foreach (Todo item in todos)
+    {
+        context.Remove(item);
+        //Console.WriteLine("Todo{0}, task : {1} completed : {2}",item.Id, item.Task, item.Completed);
+    }
+    
+    /*var afaire = context.Todo
+            .Where(b => b.Completed ==false)
+            .ToList();
+
+    foreach (Todo item in afaire)
+    {
+        item.Completed =true;
+        context.SaveChanges();
+        //Console.WriteLine("Todo{0}, task : {1} completed : {2}",item.Id, item.Task, item.Completed);
+    }*/
+
+    
+    context.SaveChanges();
+    
 }
